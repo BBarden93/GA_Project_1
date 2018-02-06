9ba29c6c26c10d1ec8523ae18aaf8e537bcc2d06
Brittany Barden <bbarden93@yahoo.com>
2018-02-06 14:26:47 -0800

    popualate answer boxes to questions list

--- trivia.js
+++ trivia.js
@@ -26,20 +26,26 @@ var questionsList = [
     a: 0,
     ptValue: 1 }
 ]
-
+//variables for time function
 var $startBtn = $('#startButton')
 var $seconds = $('#seconds')
 var time = $seconds.text()
 var timer = Number(time) 
 var countdown;
+//variables for population using question list
+var $questionBox = $('#questionBox')
+var $question = $('.question')
+var $answerBox1 = $('#answerBox1')
+var $answerBox2 = $('#answerBox2')
+var $answerBox3 = $('#answerBox3')
+var $answerBox4 = $('#answerBox4')
 
 function startClock() {
     console.log('You have clicked the start button') 
-   
-    runClock() 
+   runClock() 
+   populate()
 }
 function runClock (){
-    
     countdown = setInterval(function(){
         timer--
         console.log(timer)
@@ -47,13 +53,22 @@ function runClock (){
         $seconds.text(timer)
     }, 1000) 
 }
-
-function stopTimer() {
+function stopTimer(){
     if (timer === 0) {
     clearInterval(countdown)
     }
 }
 $('#startButton').on('click', startClock);
+
+function populate() {
+    //access question list- question goes to questions box 
+    $question.text(questionsList[0].question)
+    // options go to spans
+    $answerBox1.text(questionsList[0].options[0])
+    $answerBox2.text(questionsList[0].options[1])
+    $answerBox3.text(questionsList[0].options[2])
+    $answerBox4.text(questionsList[0].options[3])
+}
 
 /*
 function randomInt(n){
@@ -63,13 +78,5 @@ var game = {
     questions: questionsList,
     currentQuestion: null,
     currentPlayer: null,
-    
-}
-
-$questionBox = $('#questionBox')
-
-function generateRandomQuestion (){
-    $questionBox.text(questionsList[randomInt(questionsList.length)])
 }
 */
-
