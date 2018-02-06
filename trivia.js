6aeba2dd7ca7b004d1258eec7a3176eeab0bc8d3
Brittany Barden <bbarden93@yahoo.com>
2018-02-06 12:10:56 -0800

    timer logic

--- trivia.js
+++ trivia.js
@@ -1,28 +1,75 @@
-var score = 0;
 
-var game = {
-    q1: [{question: "Which line of code is correct?",
+
+var questionsList = [
+    {question: "Which line of code is correct?",
     options: ["var winner = 'me'", "var winner::me", "var: winner = me", "variable winner is me"],
-    a: [0],
-    ptValue: 1 }],
+    a: 0,
+    ptValue: 1 },
 
-    q2: [{question: "What does HTML stand for?",
+    {question: "What does HTML stand for?",
     options: ["Hyperloop Transportation Musk Line", "Hyper Transfer Markup Language", "Holy Text Mark Language", "Hyper Text Markup Language"],
-    a: [3],
-    ptValue: 1 }],
+    a: 3,
+    ptValue: 1 },
 
-    q3: [{question: "A sketch that detail the feaures and functionality of a site is a:",
+    {question: "A sketch that detail the feaures and functionality of a site is a:",
     options: ["Annotation", "Design", "Wireframe", "Domain"],
-    a: [2],
-    ptValue: 1 }],
+    a: 2,
+    ptValue: 1 },
 
-    q4: [{question: "ls, pwd, rm, and mkdir are used in what text-based interface?" ,
+    {question: "ls, pwd, rm, and mkdir are used in what text-based interface?" ,
     options: ["argument line", "command line", "directory", "terminal"],
-    a: [1],
-    ptValue: 1 }],
+    a: 1,
+    ptValue: 1 },
 
-    q5: [{question: "Git is an open source program for tracking changes in text files.",
+    {question: "Git is an open source program for tracking changes in text files.",
     options:["true", "false", "boolean", "Wouldn't you like to know"],
-    a: [0],
-    ptValue: 1 }],
+    a: 0,
+    ptValue: 1 }
+]
+
+var $startBtn = $('#startButton')
+var $seconds = $('#seconds')
+var time = $seconds.text()
+var timer = Number(time) 
+var countdown;
+
+function startClock() {
+    console.log('You have clicked the start button') 
+   
+    runClock() 
 }
+function runClock (){
+    
+    countdown = setInterval(function(){
+        timer--
+        console.log(timer)
+        stopTimer()
+        $seconds.text(timer)
+    }, 1000) 
+}
+
+function stopTimer() {
+    if (timer === 0) {
+    clearInterval(countdown)
+    }
+}
+$('#startButton').on('click', startClock);
+
+/*
+function randomInt(n){
+    return Math.floor(Math.random() * n)
+}
+var game = {
+    questions: questionsList,
+    currentQuestion: null,
+    currentPlayer: null,
+    
+}
+
+$questionBox = $('#questionBox')
+
+function generateRandomQuestion (){
+    $questionBox.text(questionsList[randomInt(questionsList.length)])
+}
+*/
+
